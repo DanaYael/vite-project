23f1ff301d5d0cb4324542dca7cc3f13dbbb2c0f
Dana Yael Albornoz <Yaelalbornoz23@gmail.com>
2024-12-13 12:57:43 -0300

    se agrego el useState

--- src/componentes/Hola.jsx
+++ src/componentes/Hola.jsx
@@ -1,9 +1,22 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
 
 const Hola = ({ amigo }) => {
+  const [msj, setMsj] = useState("");
+
+  const handleClick = () => {
+    setMsj("(from changed state)");
+  };
   return (
-    <h1 className="d-flex justify-content align-center">Hellow {amigo}</h1>
+    <div className="d-flex flex-column align-items-center">
+      <h1 className="d-flex justify-content align-center">
+        Hello {amigo} {msj}
+      </h1>
+      <button className="btn btn-primary mt-3" onClick={handleClick}>
+        Change Message
+      </button>
+    </div>
   );
 };
 
